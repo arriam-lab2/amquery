c5ff3eaddfc7f9d1c6075e27ca602fce58da3036
Nikolay Romashchenko <nikolay.romashchenko@gmail.com>
2016-03-06 22:45:49 +0000

    R: таблицы расстояний. Преобразования данных и heatmap(R-squared)

--- src/1.R
+++ src/1.R
@@ -1,17 +1,32 @@
 
-# unweighted unifrac distance table
-#uu <- read.table('data/unweighted_unifrac_otu_table.txt', sep="\t", header=TRUE)
-#print(uu)
+#filename = 'data/weighted_unifrac_otu_table.txt'
+filename = 'data/unweighted_unifrac_otu_table.txt'
+xtable <- read.table(filename, sep="\t", header=TRUE)
+uu <- as.data.frame(xtable)
 
-#'data/weighted_unifrac_otu_table.txt'
 filename = 'j_matrix.txt'
 xtable <- read.table(filename, sep="\t", header=TRUE)
-df <- as.data.frame(xtable)
+ji <- as.data.frame(xtable)
 
-df$X <- as.character(df$X)
-df$type <- substr(df$X, 1, nchar(df$X) - 1)
-df$type <- as.factor(df$type)
-df$X <- NULL
+# reorder rows and cols
+m <- match(uu$X, ji$X)
+ji$X <- NULL
+uu$X <- NULL
+ji <- ji[m,m]
 
-library(lattice)
-splom(df, col = df$type)
+# R-square matrix
+cor_matrix <- round(cor(uu, ji)^2, 4)
+library(reshape2)
+mcor_matrix<- melt(cor_matrix)
+
+# heat map
+library(ggplot2)
+ggplot(data = mcor_matrix, aes(x=Var1, y=Var2, fill=value)) + geom_tile()
+
+#uu$X <- as.character(df$X)
+#uu$type <- substr(df$X, 1, nchar(df$X) - 1)
+#uu$type <- as.factor(df$type)
+#uu$X <- NULL
+
+#library(lattice)
+#pairs(uu, col = uu$type)
