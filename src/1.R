a31fdfa1ecea6fbe3003b472e669cb29b4217e00
Nikolay Romashchenko <nikolay.romashchenko@gmail.com>
2016-03-10 22:53:02 +0000

    Автоматизация обработки многих таблиц расстояний (.R)

--- src/1.R
+++ src/1.R
@@ -1,37 +1,60 @@
 
-#filename = 'data/weighted_unifrac_otu_table.txt'
-filename = 'data/unweighted_unifrac_otu_table.txt'
-xtable <- read.table(filename, sep="\t", header=TRUE)
-uu <- as.data.frame(xtable)
-
 filename = 'jaccard_matrix.txt'
 #filename = 'jsd_matrix.txt'
 xtable <- read.table(filename, sep="\t", header=TRUE)
 ji <- as.data.frame(xtable)
 
-# reorder rows and cols
-m <- match(uu$X, ji$X)
-ji$X <- NULL
-uu$X <- NULL
-ji <- ji[m,m]
 
-# correlation matrix
-cor_matrix <- round(cor(uu, ji)^2, 4)
-library(reshape2)
-mcor_matrix<- melt(cor_matrix)
-
-# heat map
-library(ggplot2)
-ggplot(data = mcor_matrix, aes(x=Var1, y=Var2, fill=value)) + geom_tile()
-
-# R-squared function
-r2 <- function(x, y) {
-    tss = sum((x - mean(x[[1]]))^2)
-    rss = sum((y - x)^2)
-    return (1 - rss/tss)
+# parse files containing distance matrixies
+dm.parse <- function(directory) {
+    dir_files = list.files(directory, pattern='*.txt', full.names=TRUE)
+    l <- lapply(dir_files, read.table, sep="\t", header=TRUE)
+    lapply(l, as.data.frame)
 }
 
-df <- melt(mapply(r2, uu, ji))
-print(df)
-ggplot(df, aes(x=seq(1, dim(df)[1]), y=value)) + geom_line()
+# reorder rows and cols as in sample
+dm.reformat <- function(dm, sample) {
+    m <- match(sample$X, dm$X)
+    dm$X <- NULL
+    dm[m,m]
+}
+
+dm.compare.cor2 <- function(x, y) {
+    # correlation matrix
+    cor_matrix <- round(cor(x, y)^2, 4)
+    library(reshape2)
+    mcor_matrix <- melt(cor_matrix)
+
+    # cor-squared heat map
+    library(ggplot2)
+    ggplot(data = mcor_matrix, aes(x=Var1, y=Var2, fill=value)) + geom_tile()
+}
+
+dm.compare.r2 <- function(x, y) {
+    # R-squared function
+    r2 <- function(x, y) {
+        tss = sum((x - mean(x[[1]]))^2)
+        rss = sum((y - x)^2)
+        return (1 - rss/tss)
+    }
+
+    df <- melt(mapply(r2, x, y))
+    #ggplot(df, aes(x=seq(1, dim(df)[1]), y=value)) + geom_line()
+}
+
+dm.compare <- function(x, y) {
+    y <- dm.reformat(y, x)
+    x$X <- NULL
+    dm.compare.cor2(x, y)
+    dm.compare.r2(x, y)
+}
+
+#filename = 'data/weighted_unifrac_otu_table.txt'
+filename = 'data/unweighted_unifrac_otu_table.txt'
+uu_path = 'data/out2/unweighted_unifrac/rare_dm/'
+
+uu_tables <- dm.parse(uu_path)
+uu <- uu_tables[[1]]
+lapply(uu_tables, dm.compare, ji)
+#dm.compare(uu, ji)
 
