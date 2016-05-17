d556d14d4ce3d96b2c5ea38c60912569cd1babb8
Nikolay Romashchenko <nikolay.romashchenko@gmail.com>
2016-05-17 17:51:39 +0300

    Server master created (GNU parallel porting)

--- src/sh/read_filter.sh
+++ src/sh/read_filter.sh
@@ -4,7 +4,9 @@ OUTPUT_DIR="../../out/"
 MIN=109
 MAX=323
 
-if [ "$#" == 1 ] && [ -d "$1" ]; then
+echo $1
+
+if [ "$#" == 1 ]; then
     echo "Filtering" $1"..."
     python3 ../python/read_filter.py -i $1 -o $OUTPUT_DIR --min $MIN --max $MAX
 else
--- src/sh/run_read_filter.sh
+++ src/sh/run_read_filter.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 directories=(
-"../../data/AlnusCheckerboard"
-"../../data/Extracellular_DNA_in_soil"
-"../../data/FL_Spatial_Study_Complete"
-"../../data/hungate_qSIP"
-"../../data/Influence_of_soil_properties_on_microbial_diversity_in_aged_PAHs_soil"
-"../../data/IPY_Thule_Metagenomics"
-"../../data/IPY_Toolik_Metagenomics"
+"~/storage/metagen/AlnusCheckerboard"
+"~/storage/metagen/Extracellular_DNA_in_soil"
+"~/storage/metagen/FL_Spatial_Study_Complete"
+"~/storage/metagen/hungate_qSIP"
+"~/storage/metagen/Influence_of_soil_properties_on_microbial_diversity_in_aged_PAHs_soil"
+"~/storage/metagen/IPY_Thule_Metagenomics"
+"~/storage/metagen/IPY_Toolik_Metagenomics"
 )
 
 NUM_THREADS=4
 chmod +x read_filter.sh
-parallel --ungroup --will-cite -j$NUM_THREADS ./read_filter.sh ::: "${directories[@]}"
+parallel --no-notice --ungroup -j $NUM_THREADS ./read_filter.sh ::: "${directories[@]}"
+#parallel -j $NUM_THREADS ./read_filter.sh ::: "${directories[@]}"
