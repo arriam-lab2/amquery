086d138ae727a467695c9cc4310b3a895c0ea5bd
Ilia Korvigo <grayfall9@me.com>
2016-05-19 11:30:30 +0300

    Type annotation and minor improvement

--- src/python/src/lib/dist.py
+++ src/python/src/lib/dist.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 
+from typing import Generator, Iterable, Mapping
 from collections import Counter
 import zlib
 
 
-def kmerize_string(string, k, compress=9):
+def kmerize_string(string: str, k: int, compression: int=9) -> Generator[bytes]:
     byte_string = bytes(string, encoding="utf8")
-    return (zlib.compress(byte_string[i:i+k], compress)
+    return (zlib.compress(byte_string[i:i+k], compression)
             for i in range(len(string)-k+1))
 
 
-def count_kmers(strings, k):
+def count_kmers(strings: Iterable[str], k: int) -> Counter:
     return Counter(
         kmer for string in strings for kmer in kmerize_string(string, k))
 
 
-def kmerize_samples(seqs, k):
-    return {sample: count_kmers(seqs[sample].values(), k) for sample in seqs}
+def kmerize_samples(samples: Mapping, k: int):
+    return {sample: count_kmers(seqs, k) for sample, seqs in samples.items()}
 
 
 if __name__ == "__main__":
