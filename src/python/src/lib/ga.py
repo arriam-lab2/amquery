192a4df633b3b913eece3c937d99af0e8b298f4f
Ilia Korvigo <grayfall9@me.com>
2016-05-21 00:26:17 +0300

    Updated GA, multiprocessing is not working, yet

--- src/python/src/lib/ga.py
+++ src/python/src/lib/ga.py
@@ -1,44 +1,31 @@
-#! /usr/bin/env python
-
-import abc
+from typing import Sequence, Callable, Union, Any, Tuple, List
+import operator as op
 import itertools
 import random
-import time
-import functools
-from collections import Sequence, Callable
+import abc
 
+import multiprocess.pool
 import numpy as np
-import joblib.pool
 
-
-class abstractstatic(staticmethod):
-    __slots__ = ()
-
-    def __init__(self, function):
-        super(abstractstatic, self).__init__(function)
-        function.__isabstractmethod__ = True
-    __isabstractmethod__ = True
+from .work import N_JOBS
 
 
 class BaseIndividual(metaclass=abc.ABCMeta):
-    def __hash__(self):
-        return hash(self.chromosome)
-
+    @abc.abstractmethod
     def __eq__(self, other):
         """
         :type other: Individual
         :rtype: bool
         """
-        if not isinstance(other, Individual):
-            raise TypeError
-        return self.chromosome == other.chromosome
+        pass
 
+    @abc.abstractmethod
     def __ne__(self, other):
         """
         :type other: Individual
         :rtype: bool
         """
-        return not self == other
+        pass
 
     @abc.abstractproperty
     def engine(self):
@@ -49,34 +36,12 @@ class BaseIndividual(metaclass=abc.ABCMeta):
         pass
 
     @abc.abstractproperty
-    def mutation_rate(self):
+    def mutation_rate(self) -> float:
         pass
 
-    @abstractstatic
-    def _mutate(mutation_rate, chromosome, engine):
-        """
-        :type mutation_rate: float
-        :param mutation_rate: the probability of mutation per gene
-        :type chromosome: Sequence
-        :param chromosome: a sequence of genes (features)
-        :rtype: list
-        :return: a mutated chromosome
-        """
+    @abc.abstractmethod
+    def replicate(self) -> Sequence:
         pass
-
-    @abstractstatic
-    def _crossover(chr1, chr2):
-        """
-        :param chr1: a chromosome type object
-        :param chr2: a chromosome type object
-        :rtype: list
-        :return: the result of crossover between chr1 and chr2
-        """
-        pass
-
-    def replicate_chr(self):
-        return self._mutate(self.mutation_rate,
-                            self.chromosome, self.engine)
 
     @abc.abstractmethod
     def mate(self, other):
@@ -95,7 +60,7 @@ class Individual(BaseIndividual):
     :type _chromosome: tuple
     """
 
-    def __init__(self, mutation_rate, engine, l=None, starting_chr=None):
+    def __init__(self, mutation_rate, engine, l, starting_chr=None):
         # TODO add docs about starting_chromosome
         """
         :type mutation_rate: float
@@ -131,10 +96,12 @@ class Individual(BaseIndividual):
         elif l is None and not isinstance(engine, Sequence):
             raise ValueError("`l` is not specified, while `engine` is not a "
                              "sequence, hence `l` cannot be be inferred")
+        if not isinstance(mutation_rate, float):
+            raise ValueError("Mutation rate must be a float")
 
-        self._l = l if l else len(engine)
         self._engine = (engine if isinstance(engine, Sequence) else
-                        [engine] * self._l)
+                        [engine] * l)
+        self._l = l if l else len(engine)
         self._mutation_rate = mutation_rate
 
         if starting_chr and self._l != len(starting_chr):
@@ -143,8 +110,20 @@ class Individual(BaseIndividual):
                 "of features specified by `l` (or inferred from "
                 "`len(engine)`)")
         # chromosome is a sequence of genes (features)
-        self._chromosome = (tuple(starting_chr) if starting_chr else
+        self._chromosome = (starting_chr if starting_chr else
                             tuple(gen(None) for gen in self._engine))
+
+    def __eq__(self, other):
+        """
+        :type other: BaseIndividual
+        """
+        return self.chromosome == other.chromosome
+
+    def __ne__(self, other):
+        """
+        :type other: BaseIndividual
+        """
+        return not self == other
 
     @property
     def engine(self):
@@ -156,7 +135,7 @@ class Individual(BaseIndividual):
     @property
     def chromosome(self):
         """
-        :rtype: tuple
+        :rtype: tuple[Any]
         """
         return self._chromosome
 
@@ -174,27 +153,34 @@ class Individual(BaseIndividual):
         :param mutation_rate: the probability of mutation per gene
         :type chromosome: Sequence
         :param chromosome: a sequence of genes (features)
-        :rtype: list
+        :rtype: list[Any]
         :return: a mutated chromosome
         """
         mutation_mask = np.random.binomial(1, mutation_rate, len(chromosome))
-        return [generator(val) if mutate else val for val, mutate, generator in
+        return [gen(val) if mutate else val for (val, mutate, gen) in
                 list(zip(chromosome, mutation_mask, engine))]
 
     @staticmethod
     def _crossover(chr1, chr2):
         """
+        :type chr1: Sequence
+        :type chr2: Sequence
         :rtype: list
         """
         if len(chr1) != len(chr2):
             raise ValueError("Incompatible species can't mate")
         choice_mask = np.random.binomial(1, 0.5, len(chr1))
-        return [gene1 if choice else gene2 for gene1, gene2, choice in
-                list(zip(chr1, chr2, choice_mask))]
+        return [a if ch else b for (a, b, ch) in zip(choice_mask, chr1, chr2)]
+
+    def replicate(self):
+        return self._mutate(self.mutation_rate, self.chromosome, self.engine)
 
     def mate(self, other):
-        offspring_chr = self._crossover(self.replicate_chr(),
-                                        other.replicate_chr())
+        """
+        :type other: BaseIndividual
+        """
+        offspring_chr = self._crossover(self.replicate(),
+                                        other.replicate())
 
         return type(self)(mutation_rate=self._mutation_rate,
                           engine=self._engine, l=self._l,
@@ -203,8 +189,9 @@ class Individual(BaseIndividual):
 
 class Population(object):
     """
-    :type _evaluated_ancestors: list[(object, BaseIndividual)]
-    :type _legends: list[(object, BaseIndividual)]
+    :type _evaluated_ancestors: list[(Any, BaseIndividual)]
+    :type _legends: list[(Any, BaseIndividual)]
+    :type _fitness_func: (BaseIndividual) -> Any
     """
     modes = ("maximize", "minimize")
 
@@ -215,7 +202,7 @@ class Population(object):
         :param ancestors: a bunch of individuals to begin with
         :type size: int
         :param size: population size
-        :type fitness_function: (BaseIndividual) -> object
+        :type fitness_function: (BaseIndividual) -> Any
         :param fitness_function: a callable that requires one argument - an
                                  instance of Individual - and returns an
                                  instance of a class that supports comparison
@@ -263,15 +250,15 @@ class Population(object):
     @property
     def legends(self):
         """
-        :rtype: list[(object, Individual)]
+        :rtype: list[(Any, BaseIndividual)]
         """
         return self._legends
 
     def _repopulate(self, evaluated_ancestors):
         """
         Mate ancestors to restore population size
-        :type evaluated_ancestors: list[(object, Individual)]
-        :rtype: list[(object, Individual)]
+        :type evaluated_ancestors: list[(Any, BaseIndividual)]
+        :rtype: list[(Any, BaseIndividual)]
         """
         # generate all possible pairs of individuals and mate enough random
         # pairs to reach the desired population size
@@ -279,41 +266,24 @@ class Population(object):
         all_pairs = tuple(itertools.combinations(individuals, r=2))
         mating_pairs = (random.choice(all_pairs)
                         for _ in range(self._size - len(evaluated_ancestors)))
-        offsprings = [indiv1.mate(indiv2) for indiv1, indiv2 in mating_pairs]
+        evaluated_offsprings = map(self._mate_and_evaluate, mating_pairs)
         # evaluate offsprings and merge the result with `evaluated_ancestors`
-        offspring_fitness = map(self._fitness_func, offsprings)
-        return list(zip(offspring_fitness, offsprings)) + evaluated_ancestors
+        return op.iadd(list(evaluated_offsprings), evaluated_ancestors)
 
-    def _mate_and_evaluate(self, mating_pairs, fitness_func):
-        pass
-
-    def _repopulate_parallel(self, evaluated_ancestors, workers):
-        """
-        Mate ancestors to restore population size
-        :type evaluated_ancestors: list[(object, Individual)]
-        :type workers: joblib.pool.Pool
-        :rtype: list[(object, Individual)]
-        """
-        # generate all possible pairs of individuals and mate enough random
-        # pairs to reach the desired population size
-        individuals = [indiv for _, indiv in evaluated_ancestors]
-        all_pairs = tuple(itertools.combinations(individuals, r=2))
-        mating_pairs = (random.choice(all_pairs)
-                        for _ in range(self._size - len(evaluated_ancestors)))
-        offsprings = [indiv1.mate(indiv2) for indiv1, indiv2 in mating_pairs]
-        # evaluate offsprings and merge the result with `evaluated_ancestors`
-        offspring_fitness = workers.map(self._fitness_func, offsprings)
-        return list(zip(offspring_fitness, offsprings)) + evaluated_ancestors
+    def _mate_and_evaluate(self, mating_pair):
+        indiv1, indiv2 = mating_pair
+        offspring = indiv1.mate(indiv2)
+        return self._fitness_func(offspring), offspring
 
     def _select(self, evaluated_population, n_fittest, n_random_unfit):
         """
-        :type evaluated_population: list[(object, BaseIndividual)]
+        :type evaluated_population: list[(Any, BaseIndividual)]
         :param evaluated_population:
         :type n_fittest: int
         :param n_fittest:
         :type n_random_unfit: int
         :param n_random_unfit:
-        :rtype: list[object, BaseIndividual)]
+        :rtype: list[Any, BaseIndividual)]
         """
         # pick the most fittest and random lesser fit individuals
         ranked_pop = sorted(evaluated_population, reverse=self._maximize,
@@ -327,14 +297,14 @@ class Population(object):
     def _filter_duplicates_by_id(objects):
         """
         Filter duplicated references from a sequence. Utilises object ids.
-        :type objects: Sequence[object]
-        :rtype: list[object]
+        :type objects: Sequence[Any]
+        :rtype: list[Any]
         """
         return {id(obj): obj for obj in objects}.values()
 
     def _update_legends(self, contenders):
         """
-        :type contenders: list[(object, BaseIndividual)]
+        :type contenders: list[(Any, BaseIndividual)]
         :rtype: None
         """
         # merge `contenders` with `self._legends`, sort again and strip to
@@ -350,10 +320,10 @@ class Population(object):
         """
         Repopulate the population, apply selection, update the list of legends
         and return the survivors
-        :type evaluated_ancestors: list[(object, Individual)]
+        :type evaluated_ancestors: list[(Any, Individual)]
         :param n_fittest:
         :param n_unfit:
-        :rtype: list[(object, Individual)]
+        :rtype: list[(Any, Individual)]
         """
         # repopulate, apply selection and update the hall of fame (legends)
         population = self._repopulate(evaluated_ancestors)
@@ -364,19 +334,7 @@ class Population(object):
         self._update_legends(selected_individuals[:self._n_legends])
         return selected_individuals
 
-    def _run_generation_parallel(self, evaluated_ancestors, n_fittest, n_unfit,
-                                 workers, chunksize):
-        # repopulate, apply selection and update the hall of fame (legends)
-        population = self._repopulate_parallel(evaluated_ancestors, workers,
-                                               chunksize)
-        selected_individuals = self._select(population, n_fittest, n_unfit)
-        # note: since the list returned by `self._select` starts with the
-        # fittest individuals we can pick contenders for the hall of fame
-        # by slicing the first `self._n_legends` of `selected_individuals`
-        self._update_legends(selected_individuals[:self._n_legends])
-        return selected_individuals
-
-    def evolve(self, n_gen, n_fittest, n_random_unfit, n_jobs=1):
+    def evolve(self, n_gen, n_fittest, n_random_unfit):
         """
         :type n_gen: int
         :param n_gen: the number of generations
@@ -397,45 +355,16 @@ class Population(object):
             raise ValueError("Population size is too small to fit "
                              "`n_fittest` + `n_unfit`")
         current_generation = self._evaluated_ancestors
-        if n_jobs == 1:
-            for _ in range(n_gen):
-                current_generation = self._run_generation(
-                    current_generation, n_fittest, n_random_unfit)
-                yield self._legends
-            return
-        pool = joblib.pool.Pool(n_jobs)
-        chunksize = self._size // n_jobs
+
         for _ in range(n_gen):
-            current_generation = self._run_generation_parallel(
-                current_generation, n_fittest, n_random_unfit, pool, chunksize)
+            current_generation = self._run_generation(
+                current_generation, n_fittest, n_random_unfit)
             yield self._legends
-        return
-
-
-def engine(x):
-    return random.randint(0, 50)
-
-
-def fitness(x):
-    return abs(200 - sum(n ** 120 for n in x.chromosome) ** 0.05)
-
-
-def test():
-    ancestors = [Individual(0.1, engine, 100) for _ in range(2)]
-
-    population = Population(ancestors, 1000, fitness, mode="minimize")
-    start = time.time()
-    for generation_legends in population.evolve(100, 25, 10, n_jobs=1):
-        pass
-        # print(*[fitness for fitness, indiv in generation_legends])
-    print(time.time() - start)
-
-    start = time.time()
-    for generation_legends in population.evolve(100, 25, 10, n_jobs=2):
-        pass
-        # print(*[fitness for fitness, indiv in generation_legends])
-    print(time.time() - start)
 
 
 if __name__ == "__main__":
-    test()
+    raise RuntimeError
+else:
+    print("hello")
+    workers = multiprocess.pool.Pool(processes=N_JOBS)
+    print("goodbye")
