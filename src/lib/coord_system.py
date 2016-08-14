790c60e3d509f4ee3db8ced7d51f97e34839aa42
Nikolay Romashchenko <nikolay.romashchenko@gmail.com>
2016-08-14 18:38:45 +0300

    coord_system.py

---
+++ src/lib/coord_system.py
@@ -0,0 +1,10 @@
+from .config import Config
+
+class CoordSystem(list):
+    @staticmethod
+    def load(config: Config):
+        return CoordSystem()
+
+    @staticmethod
+    def calculate(config: Config):
+        return CoordSystem()
