8021cedcfc762d009fcc28e246d0d10bb86530c5
Nikolay Romashchenko <nikolay.romashchenko@gmail.com>
2016-09-03 23:23:34 +0300

    Updated setup.py

--- src/setup.py
+++ src/setup.py
@@ -5,13 +5,16 @@ setup(
     version='0.1',
     py_modules=['mgns'],
     install_requires=[
-        'click',
-        'numpy',
-        'scipy',
-        'click',
-        'biopython',
-        'joblib',
-        'tqdm'
+        'numpy>=1.11.0',
+        'scipy>=0.17.0',
+        'click>=6.6',
+        'biopython>=1.66',
+        'joblib>=0.9.4',
+        'multiprocess>=0.70.4',
+        'Comparable>=1.0',
+        'bunch>=1.0.1',
+        'genetic>=0.1.dev3',
+        'tqdm>=4.7.0'
     ],
     entry_points='''
         [console_scripts]
