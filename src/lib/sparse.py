68e1bc4e62c4a0fabf69e4b3e4bcc2d979ee574f
Nikolay Romashchenko <nikolay.romashchenko@gmail.com>
2016-09-06 17:53:57 +0300

    src.lib.sparse

---
+++ src/lib/sparse.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+class SparseArray:
+    def __init__(self, cols: np.array, data: np.array):
+        self.cols = cols
+        self.data = data
+
+    def __len__(self):
+        return len(self.cols)
