7fd8dc063a923ba932b59382c83dc503eaa0fb5e
Nikolay Romashchenko <nikolay.romashchenko@gmail.com>
2017-04-05 22:14:38 +0300

    Added split.sh file

--- data/bench.sh
+++ data/bench.sh
@@ -21,44 +21,14 @@ function runqiime {
     beta_diversity.py -i ${1}/otu_table_w_tax_merged.biom -o ${1}/bdiv -m weighted_unifrac -t ${1}/rep_set_merged.tre
 }
 
-function split {
-    output_dir="${3}/${2}"
-    mkdir -p "${output_dir}"
+function amq {
 
-    len=$(ls ${1} | wc -l)
-    abslen=`python -c "print(int(${2}*${len}))"`
-    shuffled_samples=($(ls -df ${1}/* | sort -R))
-    len=$(ls ${1} | wc -l)
-    additional_size=$(expr ${len} - ${abslen})
-
-    to_build=$(printf '%s\n' "${shuffled_samples[@]}" | head -n ${abslen})
-    to_add=$(printf '%s\n' "${shuffled_samples[@]}" | tail -n $additional_size)
-
-    main_output_subdir="${output_dir}/main"
-    additional_output_subdir="${output_dir}/additional"
-
-    mkdir -p "${main_output_subdir}"
-    mkdir -p "${additional_output_subdir}"
-
-    echo "${to_build}" | xargs cp -t "${main_output_subdir}"
-    echo "${to_add}" | xargs cp -t "${additional_output_subdir}"
-    
-    python ../amquery/utils/merge_fasta.py `echo ${to_build} | xargs` -o ${output_dir}/main.fna
-    python ../amquery/utils/merge_fasta.py `echo ${to_add} | xargs` -o ${output_dir}/additional.fna
 }
 
-
-if [[ $# -ne 2 ]]; then
+if [[ $# -ne 1 ]]; then
     echo "Usage: bash bench.sh <input-dir> <output-dir>"
 else
-
-    #ratio=(0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1)
-    #for i in "${ratio[@]}"
-    #do
-    #    echo $i
-    #    split $1 $i $2 > /dev/null
-    #done
-
     #runqiime $output_dir
-    
+    #amq init test139
+    #amq build -c 10 `find /home/ilia/storage/metagen/complete/se/ERR139 -type f -name "*.fastq.gz"`
 fi
---
+++ data/split.sh
@@ -0,0 +1,42 @@
+function split {
+    input_dir="${1}"
+    pattern="${3}"
+    build_size="${4}"
+    output_dir="${2}/${build_size}"
+    mkdir -p "${output_dir}"
+
+    len=$(find ${input_dir} -type f -name "${pattern}" | wc -l)
+    [ $len -lt $build_size ] && rm -r "${output_dir}" && \
+        echo "Couldn't create $build_size split: too few files found ($len)" && return
+
+    shuffled_samples=($(find ${input_dir} -type f -name "${pattern}" | sort -R))
+    additional_size=$(expr ${len} - ${build_size})
+    [ $additional_size -le 0 ] && additional_size=0
+
+    to_build=$(printf '%s\n' "${shuffled_samples[@]}" | head -n ${build_size})
+    to_add=$(printf '%s\n' "${shuffled_samples[@]}" | tail -n ${additional_size})
+
+    main_output_subdir="${output_dir}/main"
+    additional_output_subdir="${output_dir}/additional"
+
+    mkdir -p "${main_output_subdir}"
+    mkdir -p "${additional_output_subdir}"
+
+    echo "${to_build}" | xargs ln -s -t "${main_output_subdir}"
+    [ $additional_size -gt 0 ] && echo "${to_add}" | xargs ln -s -t "${additional_output_subdir}"
+    
+    #python ../amquery/utils/merge_fasta.py `echo ${to_build} | xargs` -o ${output_dir}/main.fna
+    #python ../amquery/utils/merge_fasta.py `echo ${to_add} | xargs` -o ${output_dir}/additional.fna
+}
+
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage: bash split.sh <input-dir> <output-dir>"
+else
+    pattern='*.fasta'
+
+    for build_size in {100..1000..100}
+    do
+        split "${1}" "${2}" "${pattern}" "${build_size}"
+    done
+fi
