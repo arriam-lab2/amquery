69990d24ef0f597a7a6e9d271d6df3f82ab0535e
Nikolay Romashchenko <nikolay.romashchenko@gmail.com>
2017-04-23 13:40:54 +0300

    amq-test.sh

--- data/amq-test.sh
+++ data/amq-test.sh
@@ -15,10 +15,10 @@ function amq_test {
     echo $build_size...
     for k in 5 7 10 15 20
     do
-        amq-test -q precision `echo ${to_test}` -r "$wu_table" -k "$k" > "${index_dir}/test_${k}.log" 
+        amq-test -q precision `echo ${to_test}` -r "$wu_table" -k "$k" > "${index_dir}/test_wu_${k}.log" 
         echo $k
     done;
-    echo " OK"
+
 }
 
 
@@ -29,6 +29,17 @@ else
     
     for build_size in {100..1000..100}
     do
-        amq_test $1 $2 $build_size "${pattern}"
+        #amq_test $1 $2 $build_size "${pattern}"
     done;
+    
+
+    for k in 5 7 10 15 20
+    do
+        cat ./*/test_wu_$k.log | cut -d' ' -f1 | xargs >> mp_at_k.txt
+        cat ./*/test_wu_$k.log | cut -d' ' -f2 | xargs >> map_at_k.txt
+        cat ./*/test_wu_$k.log | cut -d' ' -f3 | xargs >> gain_at_k.txt
+        cat ./*/test_wu_$k.log | cut -d' ' -f4 | xargs >> bp_at_k.txt
+        cat ./*/test_wu_$k.log | cut -d' ' -f5 | xargs >> bmap_at_k.txt
+        cat ./*/test_wu_$k.log | cut -d' ' -f6 | xargs >> bgain_at_k.txt
+    done
 fi
