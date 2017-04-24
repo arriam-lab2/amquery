ff94d52d7e6a922f4a8e0e44d9aca0c612e53162
Nikolay Romashchenko <nikolay.romashchenko@gmail.com>
2017-04-24 20:20:17 +0300

    Updating text

--- data/plots.R
+++ data/plots.R
@@ -64,14 +64,14 @@ time.plot <- function(df) {
     p <- ggplot(df, aes(size, value, fill=variable)) + 
         geom_bar(stat="identity", position="stack", alpha=0.7) + 
         labs(x="Number of samples", y="Time in seconds") +
-        ylim(0, 900) + 
         scale_fill_npg(guide=guide_legend(title="", ncol=1)) +
         theme_minimal() + 
-        theme(legend.position="bottom")
+        theme(legend.position="bottom") +
+        theme(plot.margin = unit(c(15, 6, 6, 6), "pt"))
     p
 }
 
-time.combined <- function(filename) {
+time.ref.combined <- function(filename) {
     df <- read.table(filename)
     df.amq <- df[c("V1", "V2", "V3")]
     df.qiime.wu <- df[c("V1", "V4", "V5", "V6")]
@@ -85,10 +85,115 @@ time.combined <- function(filename) {
     df.qiime.wu <- melt(df.qiime.wu, id.vars='size')
     df.qiime.bc <- melt(df.qiime.bc, id.vars='size')
 
+    p1 <- time.plot(df.qiime.bc) + labs(x="") + ylim(0, 900)
+    p2 <- time.plot(df.qiime.wu) + labs(y="") + ylim(0, 900)
+    p3 <- time.plot(df.amq) + labs(x="", y="") + ylim(0, 900)
 
-    p1 <- time.plot(df.qiime.bc) + labs(x="")
-    p2 <- time.plot(df.qiime.wu) + labs(y="")
-    p3 <- time.plot(df.amq) + labs(x="", y="")
+    p <- plot_grid(
+        p1,
+        p2 ,
+        p3,
+        align = 'vh',
+        labels = c("A", "B", "C"),
+        hjust = -1,
+        nrow = 1
+    )
+    p
+}
+
+time.denovo.combined <- function(filename) {
+    df <- read.table(filename)
+    df.amq <- df[c("V1", "V2", "V3")]
+    df.qiime.wu <- df[c("V1", "V4", "V5", "V6", "V7", "V8", "V9", "V10")]
+    df.qiime.bc <- df[c("V1", "V4", "V5", "V6", "V7", "V8", "V9", "V11")]
+
+    colnames(df.amq) <- c('size', 'k-mer counting', 'Tree construction')
+    qiime_columns <- c('size', 'pick_otus', 'pick_rep_set', 'align_seqs', 'filter_alignment', 
+                       'make_phylogeny', 'make_otu_table', 'beta_diversity')
+    colnames(df.qiime.wu) <- qiime_columns
+    colnames(df.qiime.bc) <- qiime_columns
+
+    df.amq <- melt(df.amq, id.vars='size')
+    df.qiime.wu <- melt(df.qiime.wu, id.vars='size')
+    df.qiime.bc <- melt(df.qiime.bc, id.vars='size')
+
+    p1 <- time.plot(df.qiime.bc) + labs(x="") + ylim(0, 7500)
+    p2 <- time.plot(df.qiime.wu) + labs(y="") + ylim(0, 7500)
+    p3 <- time.plot(df.amq) + labs(x="", y="") + ylim(0, 7500)
+
+    p <- plot_grid(
+        p1,
+        p2 ,
+        p3,
+        align = 'vh',
+        labels = c("A", "B", "C"),
+        hjust = -1,
+        nrow = 1
+    )
+    p
+}
+
+mem.plot <- function(df) {
+    p <- ggplot(df, aes(size, value, fill=variable)) + 
+        geom_bar(stat="identity", position="dodge", alpha=0.7) + 
+        labs(x="Number of samples", y="Time in seconds") +
+        scale_fill_npg(guide=guide_legend(title="", ncol=2)) +
+        theme_minimal() + 
+        theme(legend.position="bottom") +
+        theme(plot.margin = unit(c(15, 6, 6, 6), "pt"))
+    p
+}
+
+mem.ref.combined <- function(filename) {
+    df <- read.table(filename)
+    df.amq <- df[c("V1", "V2")]
+    df.qiime.wu <- df[c("V1", "V3", "V4", "V5")]
+    df.qiime.bc <- df[c("V1", "V3", "V4", "V6")]
+
+    colnames(df.amq) <- c('size', 'Overall')
+    qiime_columns <- c('size', 'pick_otus', 'make_otu_table', 'beta_diversity')
+    colnames(df.qiime.wu) <- qiime_columns
+    colnames(df.qiime.bc) <- qiime_columns
+
+    df.amq <- melt(df.amq, id.vars='size')
+    df.qiime.wu <- melt(df.qiime.wu, id.vars='size')
+    df.qiime.bc <- melt(df.qiime.bc, id.vars='size')
+
+    p1 <- mem.plot(df.qiime.bc) + labs(x="", y="Memory consumption, Mb") + ylim(0, 1500)
+    p2 <- mem.plot(df.qiime.wu) + labs(y="") + ylim(0, 1500)
+    p3 <- mem.plot(df.amq) + labs(x="", y="") + ylim(0, 1500)
+
+    p <- plot_grid(
+        p1,
+        p2 ,
+        p3,
+        align = 'vh',
+        labels = c("A", "B", "C"),
+        hjust = -1,
+        nrow = 1
+    )
+    p
+}
+
+mem.denovo.combined <- function(filename) {
+    df <- read.table(filename)
+    df.amq <- df[c("V1", "V2")]
+    df.qiime.wu <- df[c("V1", "V3", "V4", "V5", "V6", "V7", "V8", "V9")]
+    df.qiime.bc <- df[c("V1", "V3", "V4", "V5", "V6", "V7", "V8", "V10")]
+
+    colnames(df.amq) <- c('size', 'Overall')
+    qiime_columns <- c('size', 'pick_otus', 'pick_rep_set', 'align_seqs', 'filter_alignment', 
+                       'make_phylogeny', 'make_otu_table', 'beta_diversity')
+    colnames(df.qiime.wu) <- qiime_columns
+    colnames(df.qiime.bc) <- qiime_columns
+
+    df.amq <- melt(df.amq, id.vars='size')
+    df.qiime.wu <- melt(df.qiime.wu, id.vars='size')
+    df.qiime.bc <- melt(df.qiime.bc, id.vars='size')
+
+    p1 <- mem.plot(df.qiime.bc) + labs(x="", y="Memory consumption, Mb") + ylim(0, 2500)
+    p2 <- mem.plot(df.qiime.wu) + labs(y="") + ylim(0, 2500)
+    p3 <- mem.plot(df.amq) + labs(x="", y="") + ylim(0, 2500)
 
     p <- plot_grid(
         p1,
@@ -104,10 +209,19 @@ time.combined <- function(filename) {
 
 
 p1 <- precision.combined(c('wu_mp_at_k.txt', 'wu_bmp_at_k.txt', 'wu_map_at_k.txt', 'wu_bmap_at_k.txt', 'wu_gain_at_k.txt', 'wu_bgain_at_k.txt'))
-ggsave("wu_precision.tiff", p1, width=18, height=10, units="cm")
+ggsave("wu_precision.tiff", p1, width=18, height=8, units="cm")
 
 p2 <- precision.combined(c('bc_mp_at_k.txt', 'bc_bmp_at_k.txt', 'bc_map_at_k.txt', 'bc_bmap_at_k.txt', 'bc_gain_at_k.txt', 'bc_bgain_at_k.txt'))
-ggsave("bc_precision.tiff", p2, width=18, height=10, units="cm")
+ggsave("bc_precision.tiff", p2, width=18, height=8, units="cm")
 
-p3 <- time.combined('ref_time.txt')
-ggsave("ref_time.tiff", p3, width=18, height=10, units="cm")
+p3 <- time.ref.combined('ref_time.txt')
+ggsave("ref_time.tiff", p3, width=18, height=9, units="cm")
+
+p4 <- time.denovo.combined('denovo_time.txt')
+ggsave("denovo_time.tiff", p4, width=18, height=12, units="cm")
+
+p5 <- mem.ref.combined('ref_mem.txt')
+ggsave("ref_mem.tiff", p5, width=18, height=9, units="cm")
+
+p6 <- mem.denovo.combined('denovo_mem.txt')
+ggsave("denovo_mem.tiff", p6, width=18, height=10, units="cm")
