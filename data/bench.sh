a67bb6cd6e37753e9ca6988226d8d39dad31ebad
Nikolay Romashchenko <nikolay.romashchenko@gmail.com>
2017-06-30 02:49:16 +0300

    Bench.sh hotfix

--- data/bench.sh
+++ data/bench.sh
@@ -1,69 +1,63 @@
-
-function runqiime {
-    mainseq=${1}/main.fna
-    addseq=${1}/additional.fna
-
-    # initial pick
-    pick_open_reference_otus.py -i ${mainseq} -o ${1}/mainotus/ --min_otu_size 1 
-    # additional pick
-    pick_open_reference_otus.py -i ${addseq} -o ${1}/addotus/ --min_otu_size 1 -r ${1}/mainotus/new_refseqs.fna --new_ref_set_id AdditionalOTU
-    # merge OTU tables
-    merge_otu_tables.py -i ${1}/mainotus/otu_table_mc1_w_tax_no_pynast_failures.biom,${1}/addotus/otu_table_mc1_w_tax_no_pynast_failures.biom -o ${1}/otu_table_w_tax_merged.biom
-    # merge repsets
-    python merge_repsets.py ${1}/mainotus/rep_set.fna ${1}/addotus/rep_set.fna ${1}/rep_set_merged.fna
-    # align merged repset
-    parallel_align_seqs_pynast.py -i ${1}/rep_set_merged.fna -o ${1}/aln/
-    # filter alignment
-    filter_alignment.py -i ${1}/aln/rep_set_merged_aligned.fasta -o ${1}/aln/
-    # build repset phylogeny
-    make_phylogeny.py -i ${1}/aln/rep_set_merged_aligned_pfiltered.fasta -o ${1}/rep_set_merged.tre
-    # beta diversity main
-    beta_diversity.py -i ${1}/otu_table_w_tax_merged.biom -o ${1}/bdiv -m weighted_unifrac -t ${1}/rep_set_merged.tre
-}
-
 function run_amq {
     split_dir=`realpath "${1}"`
     output_dir=`realpath "${2}"`
     build_size=$3
-    pattern=$4
     index_dir="${output_dir}/${build_size}"
 
-    amq --workon "${index_dir}" init origin
+    pushd `pwd`
+    mkdir -p ${index_dir}/origin && cd ${index_dir}/origin
+    amq init
 
-    # build the index
-    # NOTE: symbolic links only
-    /usr/bin/time -v amq build \
-        $(find ${split_dir}/$build_size/main -type l -name "${pattern}" -exec readlink {} \;) \
+    # build an index
+    /usr/bin/time -v amq build ${split_dir}/$build_size/main.fna \
         > "${index_dir}/build_time.log" 2> "${index_dir}/build_memory.log"
 
     echo $build_size-samples index built
-    
+
     for add_size in {100..1000..100}
+    #for add_size in {1..5..2}
     do
         # make a copy of the current index 
         cp -r "${index_dir}/origin" "${index_dir}/$add_size"
-        amq use $add_size
-        to_add=$(find ${split_dir}/$build_size/additional -type l -name "${pattern}" -exec readlink {} \; | shuf -n $add_size | xargs realpath)
+        pushd `pwd` && cd "${index_dir}/$add_size"
 
         # add new samples
-        /usr/bin/time -v amq add `echo ${to_add}` > "${index_dir}/add_${add_size}_time.log" \
-            2> "${index_dir}/add_${add_size}_memory.log"
+        /usr/bin/time -v amq add "${split_dir}/$build_size/additional.fna" \
+            > "${index_dir}/${add_size}/add_time.log" 2> \
+            "${index_dir}/${add_size}/add_memory.log"
 
         # clean up
-        rm -r "${index_dir}/$add_size"
-        echo "${to_add}" > "${index_dir}"/add_${add_size}_list.txt
+        # rm -r "${index_dir}/$add_size"
         echo $build_size: $add_size added
+
+        popd
     done;
+
+    popd
 }
 
+function merge_fasta {
+    split_dir=$1
+    index_size=$2
+    amq_source=`python -c "import amquery; print(amquery.__file__)"`
+    amq_dir=`dirname ${amq_source}`
+    python ${amq_dir}/utils/merge_fasta.py `find ${split_dir}/${index_size}/main/ -type l -exec readlink {} \; ` -o ${split_dir}/${index_size}/main.fna
+    python ${amq_dir}/utils/merge_fasta.py `find ${split_dir}/${index_size}/additional/ -type l -exec readlink {} \; ` -o ${split_dir}/${index_size}/additional.fna
+    #python ${amq_dir}/utils/merge_fasta.py `find ${split_dir}/${index_size}/main/ -type f` -o ${split_dir}/${index_size}/main.fna
+    #python ${amq_dir}/utils/merge_fasta.py `find ${split_dir}/${index_size}/additional/ -type f` -o ${split_dir}/${index_size}/additional.fna
+}
 
 if [[ $# -ne 2 ]]; then
     echo "Usage: bash bench.sh <input-dir> <output-dir>"
 else
-    pattern='*.fasta'
-    
+    #for build_size in {100..1000..100}
+    #do
+    #    merge_fasta $1 ${build_size}
+    #done;
+
     for build_size in {100..1000..100}
+    #for build_size in 10
     do
-        run_amq $1 $2 $build_size "${pattern}"
+        run_amq $1 $2 $build_size
     done;
 fi
