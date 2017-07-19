328033269e196ab4efe3e0e1e77aa26e1f37bc13
Nikolay Romashchenko <nikolay.romashchenko@gmail.com>
2017-07-19 22:25:06 +0300

    amq-test.sh fix

--- data/amq-test.sh
+++ data/amq-test.sh
@@ -1,19 +1,23 @@
 
 function run_amq_test {
-    split_dir=`realpath "${1}"`
-    output_dir=`realpath "${2}"`
+    split_dir="${1}"
+    output_dir="${2}"
     build_size=$3
-    pattern=$4
+    bench_subdir=$4
     index_dir="${output_dir}/${build_size}"
+    bench_dir=/home/ilia/storage/metagen/bench
 
-
+    pushd `pwd` && cd "${index_dir}/origin"
+    
     for k in 1 3 5 7 10 15 20
     do
-        amq-test --workon "${index_dir}" -q minprecision \
-            $(find ${split_dir}/$build_size/main -type l -name "${pattern}" -exec readlink {} \;) \
-            -r /home/ilia/storage/metagen/bench/denovo/${build_size}/wu/weighted_unifrac_otu_table.txt \
-            -k $k > "${index_dir}/minp_$k.txt" 2> "${index_dir}/minp.log"
+        amq-test -q precision \
+            ${split_dir}/$build_size/main.fna \
+            -r ${bench_dir}/$bench_subdir/${build_size}/wu/weighted_unifrac_otu_table.txt \
+            -k $k > "${index_dir}/minp_${bench_subdir}_$k.txt" 2> "${index_dir}/minp_${bench_subdir}.log"
     done;
+
+    popd
 }
 
 
@@ -21,10 +25,9 @@ function run_amq_test {
 if [[ $# -ne 2 ]]; then
     echo "Usage: bash $0 <input-dir> <output-dir>"
 else
-    pattern='*.fasta'
-    
-    for build_size in {100..1000..100}
+    for build_size in 100 300 500 700 1000
     do
-        run_amq_test $1 $2 $build_size "${pattern}"
+        run_amq_test $1 $2 $build_size denovo
+        run_amq_test $1 $2 $build_size ref
     done;
 fi
