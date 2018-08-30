a19e65357b06ce100f581823afba80f53098fa4d
Ilia Korvigo <ilia.korvigo@gmail.com>
2018-08-30 10:42:22 +0300

    Action currying

--- misal/core/action.py
+++ misal/core/action.py
@@ -2,6 +2,7 @@ import operator as op
 import inspect
 from inspect import signature
 from typing import Callable, List, Tuple, Optional
+from collections import OrderedDict
 
 from fn.func import curried
 
@@ -13,6 +14,7 @@ VARARGS = frozenset(
     [inspect.Parameter.VAR_POSITIONAL,
      inspect.Parameter.VAR_KEYWORD]
 )
+
 
 class Action:
 
@@ -35,32 +37,54 @@ class Action:
         if docs is None:
             self._docs = [(arg, '') for arg in arguments]
         else:
-            docummented_args = set(map(op.itemgetter(0), docs))
-            missing = set(arguments) - docummented_args
-            excessive = docummented_args - set(arguments)
+            docummented_args = dict(docs)
+            missing = set(arguments) - set(docummented_args)
+            unexpected = set(docummented_args) - set(arguments)
             if missing:
                 raise ValueError(f'missing docs for [{", ".join(missing)}]')
-            if excessive:
-                raise ValueError(f'nonexistent args [{", ".join(excessive)}]')
-            self._docs = docs[:]
+            if unexpected:
+                raise ValueError(f'unexpected args [{", ".join(unexpected)}]')
+            # align documentation
+            self._docs = OrderedDict(
+                (arg, docummented_args[arg]) for arg in arguments
+            )
 
         self._save = bool(save)
 
-    # TODO analyse signature
-    # TODO implement currying
-
-    def __call__(self, *args, **kwargs):
-        # add currying logic
-        # check arguments
-        raise NotImplemented
-        # return self._action(*args, **kwargs)
+    def __call__(self, *args):
+        # # align arguments
+        # args_ = [(arg, val) for (arg, _), val in zip(self.documentation, args)]
+        # repeated = [arg for (arg, _) in args_ if arg in kwargs]
+        # if any(repeated):
+        #     raise TypeError(
+        #         f'action {self.name} got multiple values for arguments '
+        #         f'[{", ".join(repeated)}]'
+        #     )
+        # unexpected = [kwarg for kwarg in self._docs if kwarg not in self._docs]
+        # if any(unexpected):
+        #     raise TypeError(
+        #         f'action {self.name} got unexpected keyword arguments '
+        #         f'[{", ".join(unexpected)}]'
+        #     )
+        if len(args) > len(self.documentation):
+            raise TypeError(
+                f'action {self.name} takes {len(self.documentation)} '
+                f'positional arguments, but {len(args)} were given'
+            )
+        # execute underlying action if its signature is fulfilled
+        if len(args) == len(self.documentation):
+            return self.action(*args)
+        # otherwise, curry the action
+        f = self.action(*args)
+        truncated_docs = self.documentation[len(args):]
+        return type(self)(self.name, f, truncated_docs, self.save)
 
     def __str__(self):
         return f'{self.name}({", ".join(map(op.itemgetter(0), self._docs))})'
 
     @property
     def documentation(self) -> List[Tuple[str, str]]:
-        return self._docs
+        return list(self._docs.items())
 
     @property
     def save(self) -> bool:
